19f0134992e8a559685a7979e5df841a6f9ce064
metjeanjunior <metjeanjunior@gmail.com>
2017-04-25 14:09:55 -0400

    updated reduce to get rid of silly errors

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # crypto-rainbow
 
-A python script that will find the hash value for any SHA-1 hash of a 6 letter word
-	* The test cases are base 64 encoded SHA-1 hashes
-	* The reduction function consists of simply concatenating the first and last 3 letters of the hash
-	* We chain 5 times or until our reductions are less than 6 chars long
+A python script that will find the hash value for any SHA-1 hash of a 6 letter word:
+* The test cases are base 64 encoded SHA-1 hashes
+* The reduction function consists of simply concatenating the first and last 3 letters of the hash
+* We chain 5 times or until our reductions are less than 6 chars long
 
 ## Some Helpful resources:
 * [RainbowTables by Kestas Kuliukas](http://kestas.kuliukas.com/RainbowTables/)
--- reduce.py
+++ reduce.py
@@ -1,8 +1,13 @@
 import hashlib, string
 
+debug = True
+
 def reduce(plain):
 	# A the SHA-1 encryption of the plaintext
 	hashval = hashlib.sha1(plain).hexdigest()
+
+	if debug:
+		print 'hash val is ', hashval
 
 	# We now apply the reduction functions
 	# We simply take the first and last 3 letters of the hash
@@ -11,14 +16,20 @@ def reduce(plain):
 	indx = 0
 
 	while True:
-		if len(resList) >= 3:
-			indx = -indx
+		if len(resList) == 3:
+			indx = -1
 
+		if debug:
+			print 'curr indx, len', indx, len(resList)
 		if hashval[indx] in string.ascii_lowercase:
 			resList.append(hashval[indx])
 
-		indx = abs(indx) + 1
+		indx = indx+1 if indx>= 0 else indx-1
 
-		if len(resList) >= 6 or len(resList) >= indx:
+		if len(resList) >= 6 or indx>= len(plain):
+			if debug:
+				print 'finished with indx', indx
 			return ''.join(resList)
 
+if debug:			
+	print reduce('test me')
